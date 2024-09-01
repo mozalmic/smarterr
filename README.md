b142d319bd07a565859073e0e00da99e1973b334
mozalmic <11075784+mozalmic@users.noreply.github.com>
2024-09-01 16:48:28 +0200

    Add documentation and tests, fix issues

--- README.md
+++ README.md
@@ -5,11 +5,27 @@
 With **_SmartErr_** it is possible to:
 
 * raise errors with `raise` and `throw` methods on regular types (numbers, strings, boolean, _Option_, _Result_, etc) as an error source. Look into [Raising errors](#raising-errors) section to find out more details.
+* define atomic errors with `error!` macro (requires `atomic_error` feature).
 * define the exact set of errors emitted by the function or introduce global set for the public API (requires `erorrset` feature).
 
-## Quick overview
+## Defining atomic errors
 
-See [this](#example) example below.
+With `atomic_error` feature enabled (it is also included into `smarterr` feature), it is possible to define atomic errors, which are structs, annotated with `thiserror::Error` attribute. For instance, the following definition:
+
+```rust,ignore
+error!(pub InvalidData<ParseIntError> { pub data: String } => "Invalid data provided [data={data}]");
+```
+
+will generate the code:
+```rust,ignore
+#[derive(thiserror::Error, Debug)]
+#[error("Invalid data provided [data={data}]")]
+pub struct InvalidData {
+    pub data: String,
+    #[source]
+    pub source: ParseIntError,
+}
+```
 
 ## Raising or throwing errors
 
@@ -29,8 +45,35 @@ Further actions depend on the function used to convert the value into _Result_:
 
 **raise** - CHANGES error context. When the error state is detected, the original value is wrapped into _Result::Ok_. Otherwise, the value is mapped with the provided `ok_to_err_map` function and returned as `Err(ok_to_err_map(original_value))`.
 
-## Defining errors
+## Defining output error set
 
-With `errorset` feature enabled, it is possible to define a set of errors emitted by the function. The `errorset` macro generates a new error type that contains all the errors from the function signature. You may find more details in the [ErrorSet crate](https://crates.io/crates/errorset) documentation.
+With `errorset` feature enabled, it is possible to define a set of errors emitted by the function. The `errorset` macro generates a new error type that contains all the errors from the function signature. 
+
+You may find more details in the [ErrorSet crate](https://crates.io/crates/errorset) documentation.
 
 `SmartErr` crate reexports `ErrorSet` crate, so you may use it functionality directly.
+
+For instance, the following code:
+
+```rust,ignore
+#[errorset]
+pub fn parse_u32(data: &str) -> Result<u32, (InvalidData, OutOfBounds)> {
+    ...
+}
+```
+
+will produce the code:
+```rust,ignore
+#[derive(::thiserror::Error, Debug)]
+pub enum ParseU32Errors {
+    #[error(transparent)]
+    InvalidData(#[from] InvalidData),
+    #[error(transparent)]
+    OutOfBounds(#[from] OutOfBounds),
+}
+impl _SomeStruct {
+    fn parse_u32(data: &str) -> Result<u32, ParseU32Errors> {
+        ...
+    }
+}
+```
--- tests/test.rs
+++ tests/test.rs
@@ -1,41 +1,75 @@
-#[cfg(feature = "atomic_error")]
+use errorset::errorset;
+use smarterr::Throwable;
+
 mod atomic_error {
     use smarterr::error;
     use std::num::ParseIntError;
 
-    // define several atomic struct-based error types using the `thiserror` crate
-    error!(pub Error1<ParseIntError> { pub data: String } => "Error1 [data={data}]");
-    error!(pub Error2 { pub message: String } => "Error2 [msg={message}]");
-    error!(pub Error3 { pub data: u32, pub message: String } => "Error3 [data={data}, msg={message}]");
-    error!(Error4 { pub data: u32, pub message: String } => "Error4 [data={data}, msg={message}]");
-}
-
-#[cfg(test)]
-#[cfg(feature = "atomic_error")]
-mod tests {
-    use smarterr::Throwable;
-
-    fn error_producer(v: &str, inverse: bool) -> Result<usize, atomic_error::Error1> {
-        let v = v
-            .parse::<usize>()
-            .throw(|e| atomic_error::Error1 { data: v.to_owned(), source: v })?;
-        if inverse {
-            v.raise(|v| atomic_error::Error2 { message: format!("Int value: {}", v) })
-        } else {
-            v.throw(|v| atomic_error::Error2 { message: format!("Int value: {}", v) })
+    #[derive(Debug)]
+    pub enum Bounds {
+        Lower(u32),
+        Upper(u32),
+    }
+    impl Bounds {
+        pub fn test(&self, value: u32) -> bool {
+            match self {
+                Bounds::Lower(bound) => value >= *bound,
+                Bounds::Upper(bound) => value <= *bound,
+            }
         }
     }
 
+    // define several atomic struct-based error types using the `thiserror` crate
+    error!(pub InvalidData<ParseIntError> { pub data: String } => "Invalid data provided [data={data}]");
+    error!(pub OutOfBounds { pub value: u32, pub bound: Bounds } => "Value is out of bound [value={value}, bound={bound:?}]");
+}
+
+use atomic_error::*;
+
+struct _SomeStruct {}
+
+#[errorset(pub(crate) mod eei)]
+impl _SomeStruct {
+    #[errorset]
+    pub fn parse_u32(&self, data: &str) -> Result<u32, (InvalidData, OutOfBounds)> {
+        let min = Bounds::Lower(100);
+        let max = Bounds::Upper(1000);
+        let data = data.to_owned();
+        let value = data.parse::<u32>().throw(|source| InvalidData { data, source })?;
+        min.test(value).throw(|_| OutOfBounds { value, bound: min })?;
+        max.test(value).throw(|_| OutOfBounds { value, bound: max })?;
+        Ok(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::anyhow;
+
     #[test]
-    fn test_error_producer() {
-        let v = "123";
-        match error_producer(v, false) {
-            Ok(_) => println!("No error occurred"),
-            Err(e) => println!("Error: {:#?}", e),
+    fn test() {
+        let s = _SomeStruct {};
+        let mut actual = vec![];
+        for value in &["x1", "10", "100", "1000", "10000"] {
+            println!("Value: {}", value);
+            let t = s.parse_u32(value);
+            let output = match t {
+                Ok(v) => format!("-- Value: {}", v),
+                Err(e) => format!("-- method_one failed: {:#}", anyhow!(e)),
+            };
+            println!("{}", output);
+            actual.push(output);
         }
-        match error_producer(v, true) {
-            Ok(_) => println!("No error occurred"),
-            Err(e) => println!("Error: {:#?}", e),
-        }
+        assert_eq!(
+            actual,
+            vec![
+                "-- method_one failed: Invalid data provided [data=x1]: invalid digit found in string",
+                "-- method_one failed: Value is out of bound [value=10, bound=Lower(100)]",
+                "-- Value: 100",
+                "-- Value: 1000",
+                "-- method_one failed: Value is out of bound [value=10000, bound=Upper(1000)]",
+            ]
+        );
     }
 }
